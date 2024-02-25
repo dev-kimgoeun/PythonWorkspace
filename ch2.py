7f5a17530442085d76b948e04c7288674fe30680
kim gon <ak0150a01@gmail.com>
2024-02-25 16:46:32 +0900

    자료형과 변수

---
+++ ch2.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+plt.rcParams['font.family'] = 'Malgun Gothic'
+
+
+print(5)
+print(-10)
+print(3.14)
+print(1000)
+
+print(5+3)
+print(2*8)
+print(6/3)
+print(3*(3+1))
+
+print(5>10)
+print(5<10)
+
+print(not(5>10))
+
+name="연탄이" #이름
+animal="개" #종류
+age=4 #나이
+hobby="산책" #취미
+
+print("반려동물을 소개해 주세요")
+print("우리집 반려동물은" +animal+ "인데, 이름은"+name+  "이예요")
+print("연탄이는 4살이고, 산책을 아주 좋아해요")
+
+print(int("3"))
+print(float("3.5"))
+print(float(3))
+
+print(str(3)+"입니다")
+print(str(3.5)+"입니다.")
