18f881196d967e362a4d22dcc84d5777614a6060
kim gon <ak0150a01@gmail.com>
2024-03-01 15:14:40 +0900

    연산자 셀프체크

--- exam1.py
+++ exam1.py
@@ -2,3 +2,9 @@ from random import *
 
 date = randint(4, 28)
 print("오프라인 스터디 모임 날짜는 매월 " + str(date) + "일로 선정되었습니다.")
+
+
+celsius = 30
+fahrenheit = (celsius * 9 /5) +32
+print("섭씨온도 : " + str(celsius))
+print("화씨온도 : "+ str(fahrenheit))
