92eea37dd9eacf3ac459415e289f70e18e63126c
kim gon <ak0150a01@gmail.com>
2024-03-09 21:56:03 +0900

    함수2

--- ch7.py
+++ ch7.py
@@ -40,18 +40,75 @@ profile("찰리",22)
 profile("찰리",24, "자바")
 profile("루시")
 
-def language(name, age, lang1, lang2, lang3, lang4, lang5) :
+def language(name, age, *code) :
     print("이름: {0}\t나이 : {1}\t".format(name, age), end=" ")
-    print(lang1, lang2, lang3, lang4, lang5)
+    #print(language, type(language))
+    for lang in code :
+        print(code, end=" ")
+        print()
 
-language("찰리", 20, "파이썬", "자바","c","c++","c#")
+language("찰리", 20, "파이썬", "자바","c","c++","c#", "자바스크립트")
 language("루시", 25, "코틀린","스위프트","","","")
 
 
+#함수안에 함수 호출
 
+def add(item):
+    print(item, "붓기")
 
+def americano():
+    add("뜨거운 물")
+    add("에스프레소")
 
+print("아메리카노 만드는 법")
+americano()
 
+#3d안경
 
+glasses = 10
 
+def rent(people):
+    global glasses
+    glasses = glasses - people
+    print("[함수 내부] 남은 3D 안경 개수 : {0}".format(glasses))
 
+print("전체 3D 안경 개수 : {0}".format(glasses))
+rent(2)
+print("남은 3D 안경 개수: {0}".format(glasses))
+
+def rent_return(glasses, people) :
+    glasses = glasses-people
+    print("[함수 내부] 남은 3D 안경 개수: {0}".format(glasses))
+    return glasses
+
+print("전체 3D 안경 개수 : {0}".format(glasses))
+glasses = rent_return(glasses, 2)
+print("남은 3D 안경 개수 :{0}".format(glasses))
+
+#표준체중 
+def std_weight(height, gender):
+    if gender == "남자":
+        return height * height * 22
+    else :
+        return height * height * 21
+    
+height = 175
+gender = "남자"
+
+weight = round(std_weight(height/ 100, gender), 2)
+print("키 {0}cm {1}의 표준 체중은 {2}kg입니다.".format(height, gender, weight))
+
+#미세먼지
+
+def get_air_quality (condition) :
+    if (condition <= 30) :
+        return "좋음"
+    elif (condition <= 80):
+        return "보통"
+    elif(condition <= 150):
+        return "나쁨"
+    else :
+        return "매우나쁨"
+    
+print(get_air_quality(15))
+print(get_air_quality(85))
