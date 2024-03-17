4617d652958fe96c29cfcc69f3a59bd056d38bcf
kim gon <ak0150a01@gmail.com>
2024-03-17 23:17:33 +0900

    모듈과 패키지

--- ch11.py
+++ ch11.py
@@ -1,4 +1,13 @@
 import thrater_module as mv
+import travel.thailand
+from travel import vietnam
+
 mv.price(3)
 mv.price_morning(4)
 mv.price_soldier(5)
+
+trip_to = travel.thailand.ThailandPackage()
+trip_to.detail()
+
+trip_to = vietnam.VietnamPackage()
+trip_to.detail()
