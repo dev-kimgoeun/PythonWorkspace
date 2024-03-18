c0d0adb9c9dc12bd426311e4143938152dc385c2
kim gon <ak0150a01@gmail.com>
2024-03-18 12:55:22 +0900

    모듈 마무리

---
+++ byme.py
@@ -0,0 +1,3 @@
+def sign():
+    print("이 프로그램은 dev-goeunkim이 만들었습니다.")
+    print("깃 : https://github.com/dev-kimgoeun")
---
+++ greeting.py
@@ -0,0 +1,12 @@
+import byme
+
+def say_hello(to):
+    print(f"안뇽, {to}?")
+def say_goodbye(to):
+    print(f"잘가, {to}!")
+
+if __name__ == "__main__":
+    say_hello("c언어")
+    say_goodbye("파이썬")
+
+byme.sign()
